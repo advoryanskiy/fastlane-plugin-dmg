6359fa1d4baab3ae03ec823cf420b4e69f1a22d0
Ed Wellbrook <edwellbrook@gmail.com>
2019-08-17 00:56:15 +0100

    Escape volume name

--- lib/fastlane/plugin/dmg/actions/dmg_action.rb
+++ lib/fastlane/plugin/dmg/actions/dmg_action.rb
@@ -26,7 +26,7 @@ module Fastlane
         Dir.mktmpdir('fldmg') do |dir|
           FileUtils.cp_r(input_path, dir)
 
-          Actions.sh "hdiutil create -fs #{params[:filesystem]} -volname #{params[:volume_name]} -srcfolder #{File.expand_path(input_path).shellescape} -ov -format #{params[:format]} -size #{params[:size]}m #{absolute_output_path.shellescape}"
+          Actions.sh "hdiutil create -fs #{params[:filesystem]} -volname #{params[:volume_name].shellescape} -srcfolder #{File.expand_path(input_path).shellescape} -ov -format #{params[:format]} -size #{params[:size]}m #{absolute_output_path.shellescape}"
         end
 
         UI.success "Successfuly generated dmg image at path #{absolute_output_path}"
