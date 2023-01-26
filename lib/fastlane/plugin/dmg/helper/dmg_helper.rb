6ba454b6be6647d75dc945530ca368f2a1bbfb6d
Alexey Dvoryanskiy <advoryanskiy@gmail.com>
2023-01-27 01:37:32 +0400

    Implemented /Applications symlink creation. Updated project to latest fastlane and libs. Debugged and fixed issues

--- lib/fastlane/plugin/dmg/helper/dmg_helper.rb
+++ lib/fastlane/plugin/dmg/helper/dmg_helper.rb
@@ -1,4 +1,8 @@
+require 'fastlane_core/ui/ui'
+
 module Fastlane
+  UI = FastlaneCore::UI unless Fastlane.const_defined?(:UI)
+
   module Helper
     class DmgHelper
       def self.folder_size(folder)
