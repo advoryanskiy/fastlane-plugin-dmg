6888ceec7dc273880113acf1112048e9d600f28d
Alexey Dvoryanskiy <advoryanskiy@gmail.com>
2023-05-23 18:40:31 +0400

    Fixed issue with coping content of .app, instead of copy .app itself

--- lib/fastlane/plugin/dmg/actions/dmg_action.rb
+++ lib/fastlane/plugin/dmg/actions/dmg_action.rb
@@ -28,8 +28,9 @@ module Fastlane
         absolute_output_dir = File.expand_path("..", absolute_output_path)
         FileUtils.mkdir_p(absolute_output_dir)
 
-        # Append "/." to path if the path doesn't end with it
-        unless input_path.end_with?("/.")
+        # Append "/." to path if the path doesn't end with it, but do this only if folder provided, no .app
+        unless File.extname(input_path) == ".app" or input_path.end_with?("/.")
+          UI.message("not .app provided, adding /.")
           input_path += "/."
         end
 
